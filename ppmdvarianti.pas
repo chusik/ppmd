66c6b4b3a21b4d1af8df5192983785c44ef2750a
chusik <alexx2000@mail.ru>
2014-10-31 07:10:20 +0300

    Update

--- ppmdvarianti.pas
+++ ppmdvarianti.pas
@@ -45,6 +45,23 @@ function NextPPMdVariantIByte(self: PPPMdModelVariantI): cint; cdecl;
 
 implementation
 
+procedure RestartModel(self: PPPMdModelVariantI); forward;
+
+procedure UpdateModel(self: PPPMdModelVariantI; mincontext: PPPMdContext); forward;
+function CreateSuccessors(self: PPPMdModelVariantI; skip: cbool; p1: PPPMdState; mincontext: PPPMdContext): PPPMdContext; forward;
+function ReduceOrder(self: PPPMdModelVariantI; state: PPPMdState; startcontext: PPPMdContext): PPPMdContext; forward;
+procedure RestoreModel(self: PPPMdModelVariantI; currcontext, mincontext, FSuccessor: PPPMdContext); forward;
+
+procedure ShrinkContext(self: PPPMdContext; newlastindex: cint; scale: cbool; model: PPPMdModelVariantI); forward;
+function CutOffContext(self: PPPMdContext; order: cint; model: PPPMdModelVariantI): PPPMdContext; forward;
+function RemoveBinConts(self: PPPMdContext; order: cint; model: PPPMdModelVariantI): PPPMdContext; forward;
+
+procedure DecodeBinSymbolVariantI(self: PPPMdContext; model: PPPMdModelVariantI); forward;
+procedure DecodeSymbol1VariantI(self: PPPMdContext; model: PPPMdModelVariantI); forward;
+procedure DecodeSymbol2VariantI(self: PPPMdContext; model: PPPMdModelVariantI); forward;
+
+procedure RescalePPMdContextVariantI(self: PPPMdContext; model: PPPMdModelVariantI); forward;
+
 function CreatePPMdModelVariantI(input: PInStream; suballocsize: cint;
   maxorder: cint; restoration: cint): PPPMdModelVariantI; cdecl;
 var
@@ -75,7 +92,67 @@ begin
 
 end;
 
+procedure RestartModel(self: PPPMdModelVariantI);
+begin
+
+end;
+
 function NextPPMdVariantIByte(self: PPPMdModelVariantI): cint; cdecl;
+begin
+
+end;
+
+procedure UpdateModel(self: PPPMdModelVariantI; mincontext: PPPMdContext);
+begin
+
+end;
+
+function CreateSuccessors(self: PPPMdModelVariantI; skip: cbool; p1: PPPMdState; mincontext: PPPMdContext): PPPMdContext;
+begin
+
+end;
+
+function ReduceOrder(self: PPPMdModelVariantI; state: PPPMdState; startcontext: PPPMdContext): PPPMdContext;
+begin
+
+end;
+
+procedure RestoreModel(self: PPPMdModelVariantI; currcontext, mincontext, FSuccessor: PPPMdContext);
+begin
+
+end;
+
+procedure ShrinkContext(self: PPPMdContext; newlastindex: cint; scale: cbool; model: PPPMdModelVariantI);
+begin
+
+end;
+
+function CutOffContext(self: PPPMdContext; order: cint; model: PPPMdModelVariantI): PPPMdContext;
+begin
+
+end;
+
+function RemoveBinConts(self: PPPMdContext; order: cint; model: PPPMdModelVariantI): PPPMdContext;
+begin
+
+end;
+
+procedure DecodeBinSymbolVariantI(self: PPPMdContext; model: PPPMdModelVariantI);
+begin
+
+end;
+
+procedure DecodeSymbol1VariantI(self: PPPMdContext; model: PPPMdModelVariantI);
+begin
+
+end;
+
+procedure DecodeSymbol2VariantI(self: PPPMdContext; model: PPPMdModelVariantI);
+begin
+
+end;
+
+procedure RescalePPMdContextVariantI(self: PPPMdContext; model: PPPMdModelVariantI);
 begin
 
 end;
